8bdc3e23c915c5cb523ec6056ba105f04f29a4aa
officialmubashir77 <officialmubashir77@gmail.com>
2026-08-14 01:23:24 +0500

    setting up enviroment of js

---
+++ setting-up-environment/test.js
@@ -0,0 +1,8 @@
+console.log("Mubashir");
+
+// Node.js is a javascript runtime environment.
+// Node.js comes with a package manager called npm.
+// Intall Node.js to run javascript code on your machine or terminal.
+
+// run this code in terminal
+// node filename.js
